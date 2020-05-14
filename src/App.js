1607c4ffe07daaea52a61ba0ebc634eb05e20d08
jokale <kalemajoanna@gmail.com>
2020-05-14 13:27:44 +0100

    Done.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import './App.css';
- 
- 
+
 class App extends Component {
- 
-  handleOnClickItems() {
-    this.props.store.dispatch({
-      type: 'GET_COUNT_OF_ITEMS',
+
+  handleOnClick() {
+    this.props.dispatch({
+      type: 'INCREASE_COUNT',
     });
   }
- 
-  handleOnClickUsers() {
-    this.props.store.dispatch({
-      type: 'GET_COUNT_OF_USERS',
-    })
-  }
- 
+
   render() {
     debugger;
     return (
       <div className="App">
-          <button onClick={() => this.handleOnClickItems()}>
-            Click to change items count
-            </button>
-          <button onClick={() => this.handleOnClickUsers()}>
-            Click to change user count
-          </button>
-          <p> {this.props.items.length}</p>
+        <button onClick={(event) => this.handleOnClick()}>
+          Click
+        </button>
+        <p>{this.props.items.length}</p>
       </div>
     );
   }
-}
- 
-const mapStateToProps = (state) => {
-  return { items: state.items }
-}
- 
+};
 
-export default connect(mapStateToProps)(App);
+// const mapStateToProps = (state) => {
+//   return { items: state.items };
+// };
+
+
+// export default App;
+const vanilla = (milkshake) => {
+  debugger;
+  return { items: milkshake.items }
+}
  
+export default connect(vanilla)(App);
+
+export default connect( state => ({ items: state.items }) )(App);
